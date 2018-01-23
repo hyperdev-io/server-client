552f55e7eefe4a2fa195a037817d73c679e2456f
Greyarch BV <gantcho@greyarch.net>
2018-01-23 15:55:08 +0100

    added createOrUpdate

--- src/apps.js
+++ src/apps.js
@@ -1,16 +1,28 @@
-const { appsQuery } = require('./queries')
-const { removeApp } = require('./mutations')
+const { appsQuery } = require("./queries");
+const { removeApp, createOrUpdateApp } = require("./mutations");
 
 module.exports = apolloClient => ({
-    list: () => new Promise((resolve, reject) => {
-        apolloClient.query({ query: appsQuery })
-            .then(res => resolve(res.data.apps))
-            .catch(error => rject(error));
+  list: () =>
+    new Promise((resolve, reject) => {
+      apolloClient
+        .query({ query: appsQuery })
+        .then(res => resolve(res.data.apps))
+        .catch(error => rject(error));
     }),
-    remove: (name, version) => new Promise((resolve, reject) => {
-        const variables = { name, version }
-        apolloClient.mutate({ mutation: removeApp, variables })
-            .then(res => resolve(res.data.removeApp))
-            .catch(reject)
+  createOrUpdate: (name, version, dockerCompose, bigboatCompose) =>
+    new Promise((resolve, reject) => {
+      const variables = { name, version, dockerCompose, bigboatCompose };
+      apolloClient
+        .mutate({ mutation: createOrUpdateApp, variables })
+        .then(res => resolve(res.data.createOrUpdateApp))
+        .catch(reject);
     }),
-})
+  remove: (name, version) =>
+    new Promise((resolve, reject) => {
+      const variables = { name, version };
+      apolloClient
+        .mutate({ mutation: removeApp, variables })
+        .then(res => resolve(res.data.removeApp))
+        .catch(reject);
+    })
+});
--- src/mutations.js
+++ src/mutations.js
@@ -1,40 +1,72 @@
-const gql = require('graphql-tag')
+const gql = require("graphql-tag");
 
 module.exports = {
-    startInstance: gql`
-        mutation startInstance($name: String!, $appName: String!, $appVersion: String!, $parameters: JSON, $options: Options) {startInstance(
-            name: $name
-            appName: $appName
-            appVersion: $appVersion
-            parameters: $parameters
-            options: $options
-        ){
-            id
-        }}
-    `,
-    stopInstance: gql`
-        mutation stopInstance($name: String!){
-            stopInstance(name: $name) {
-                id, name
-            }
-        }
-    `,
-    removeApp: gql`
-        mutation removeApp(
-            $name: String!,
-            $version: String!
-        ) {removeApp(name:$name, version:$version)}
-    `,
-    deleteBucket: gql`
-        mutation deleteBucket($name: String!){
-            deleteBucket(name: $name)
-        }
-    `,
-    copyBucket: gql`
-        mutation copyBucket($sourceName: String!, $destinationName: String!){
-            copyBucket(sourceName: $sourceName, destinationName: $destinationName) {
-                id, name, isLocked
-            }
-        }
-    `,
-}
+  startInstance: gql`
+    mutation startInstance(
+      $name: String!
+      $appName: String!
+      $appVersion: String!
+      $parameters: JSON
+      $options: Options
+    ) {
+      startInstance(
+        name: $name
+        appName: $appName
+        appVersion: $appVersion
+        parameters: $parameters
+        options: $options
+      ) {
+        id
+      }
+    }
+  `,
+  stopInstance: gql`
+    mutation stopInstance($name: String!) {
+      stopInstance(name: $name) {
+        id
+        name
+      }
+    }
+  `,
+  createOrUpdateApp: gql`
+    mutation createOrUpdateApp(
+      $name: String!
+      $version: String!
+      $dockerCompose: String!
+      $bigboatCompose: String!
+    ) {
+      createOrUpdateApp(
+        name: $name
+        version: $version
+        dockerCompose: $dockerCompose
+        bigboatCompose: $bigboatCompose
+      ) {
+        id
+        name
+        version
+        dockerCompose
+        bigboatCompose
+        tags
+      }
+    }
+  `,
+  removeApp: gql`
+    mutation removeApp($name: String!, $version: String!) {
+      removeApp(name: $name, version: $version)
+    }
+  `,
+  deleteBucket: gql`
+    mutation deleteBucket($name: String!) {
+      deleteBucket(name: $name)
+    }
+  `,
+  copyBucket: gql`
+    mutation copyBucket($sourceName: String!, $destinationName: String!) {
+      copyBucket(sourceName: $sourceName, destinationName: $destinationName) {
+        id
+        name
+        isLocked
+      }
+    }
+  `
+};
